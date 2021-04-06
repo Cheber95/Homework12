7f63a94a4613cb70c24c5af67b182383075c52c3
Cheber <sdgfqasdf46@yandex.ru>
2021-04-07 01:04:25 +0300

    v1

--- src/main/java/MyApp.java
+++ src/main/java/MyApp.java
@@ -1,63 +1,65 @@
-import java.util.concurrent.Callable;
-
 public class MyApp {
 
-    static class MyRunnableClass implements Runnable {
+    static final int SIZE = 10_000_000;
+    static final int HALF = SIZE/2;
+    private float[] arr;
 
-        @Override
-        public void run() {
-        }
+    public MyApp(float[] arr) {
+        this.arr = arr;
     }
 
-    static final int SIZE = 10000000;
-    static final int HALF = SIZE/2;
-    private static float[] arr = new float[SIZE];
-
     public static void main(String[] args) {
-        
-        MyApp app = new MyApp();
-        for (int i = 0; i < SIZE; i++) {
+        float[] stArray = new float[SIZE];
+        MyApp app = new MyApp(stArray);
+        app.play();
+    }
+
+    public void play(){
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = 1f;
         }
-        app.method1();
-        for (int i = 0; i < SIZE; i++) {
+        method1();
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = 1f;
         }
-        app.method2();
+        method2();
     }
 
     public void method1(){
         long a = System.currentTimeMillis();
-        arr = MyApp.workArray(arr);
+        workArray();
         long b = System.currentTimeMillis();
         System.out.printf("Метод 1 отработал за %d милисекунд\n", (b-a));
     }
 
+
     public void method2(){
         long a = System.currentTimeMillis();
-        float[] arrHalf1 = new float[HALF];
-        float[] arrHalf2 = new float[HALF];
-        System.arraycopy(arr,0,arrHalf1,0,HALF);
-        System.arraycopy(arr,HALF,arrHalf2,0,HALF);
+        int part = HALF;
+        float[] arrHalf1 = new float[arr.length - part];
+        float[] arrHalf2 = new float[part];
+        System.arraycopy(arr,0,arrHalf1,0,arrHalf1.length);
+        System.arraycopy(arr,arrHalf1.length,arrHalf2,0,arrHalf2.length);
 
-        Thread t1 = new Thread( () -> MyApp.workArray(arrHalf1) );
+
+        Thread t1 = new Thread( () -> new MyApp(arrHalf1).workArray() );
         t1.start();
-        Thread t2 = new Thread( () -> MyApp.workArray(arrHalf2) );
+        Thread t2 = new Thread( () -> new MyApp(arrHalf2).workArray() );
         t2.start();
 
-        while (!(t1.isAlive() && t2.isAlive())) {
-
+        while (t1.isAlive() || t2.isAlive()) {
+            continue;
         }
-        System.arraycopy(arrHalf1,0,arr,0,HALF);
-        System.arraycopy(arrHalf2,0,arr,HALF,HALF);
+        System.arraycopy(arrHalf1,0,arr,0,arrHalf1.length);
+        System.arraycopy(arrHalf2,0,arr,arrHalf1.length,arrHalf2.length);
         long b = System.currentTimeMillis();
         System.out.printf("Метод 2 отработал за %d милисекунд\n", (b-a));
     }
 
-    private static float[] workArray(float[] inArray) {
-        for (int i = 0; i < inArray.length; i++) {
-            inArray[i] = (float)(inArray[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+    private void workArray() {
+        for (int i = 0; i < arr.length; i++) {
+            float j = (float) i;
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + j / 5) * Math.cos(0.2f + j / 5) * Math.cos(0.4f + j / 2));
         }
-        return inArray;
     }
 }
